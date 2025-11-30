39edc1e3d125cd79482db5499ce6bb291d3afc97
Osama <elsultanmasr@gmail.com>
2025-11-30 14:35:30 +0200

    Lesson3

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const mongoose = require("mongoose");
 
 app.get("/", (req, res) => {
   res.sendFile("./views/home.html", { root: __dirname });
 });
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}`);
-});
+mongoose
+  .connect(
+    "mongodb+srv://elsultanmasr_db_user:DBpassword123@cluster0.xcbgw1z.mongodb.net/?appName=Cluster0"
+  )
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB", err);
+  });
