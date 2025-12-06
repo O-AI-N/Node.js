b7a483f6ec5cc355ec429a833dab0b773187c356
Osama <elsultanmasr@gmail.com>
2025-12-07 00:29:18 +0200

    Final

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 const mongoose = require("mongoose");
 app.use(express.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
 var methodOverride = require("method-override");
 app.use(methodOverride("_method"));
 const allRoutes = require("./routes/allRoutes");
