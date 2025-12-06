8ac9be75867f009d7122292e3e3b4025b85b8b8c
Osama <elsultanmasr@gmail.com>
2025-12-07 00:18:35 +0200

    Lesson 19 MVC

--- routes/allRoutes.js
+++ routes/allRoutes.js
@@ -1,62 +1,21 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/customerSchema");
-var moment = require("moment");
-// prettier-ignore
-const countries = ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda", "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cabo Verde", "Cambodia", "Cameroon", "Canada", "Central African Republic", "Chad", "Chile", "China", "Colombia", "Comoros", "Congo", "Costa Rica", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Democratic Republic of the Congo", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Eswatini", "Ethiopia", "Fiji", "Finland", "France", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Grenada", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Honduras", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Korea", "North Macedonia", "Norway", "Oman", "Pakistan", "Palau", "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Qatar", "Romania", "Russia", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Korea", "South Sudan", "Spain", "Sri Lanka", "Sudan", "Suriname", "Sweden", "Switzerland", "Syria", "Tajikistan", "Tanzania", "Thailand", "Timor-Leste", "Togo", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City", "Venezuela", "Vietnam", "Yemen", "Zambia", "Zimbabwe"]
+const userController = require("../controller/userController");
 
 // GET Requests
-router.get("/", (req, res) => {
-  //result => array of objects
-  User.find()
-    .then((result) => res.render("index", { data: result, moment }))
-    .catch((err) => console.error(err));
-});
+router.get("/", userController.user_index_get);
 
-router.get("/edit/:id", (req, res) => {
-  User.findById(req.params.id)
-    .then((result) => {
-      res.render("user/edit", { data: result, moment, countries });
-    })
-    .catch((err) => console.error(err));
-});
+router.get("/edit/:id", userController.user_edit_get);
 
-router.get("/view/:id", (req, res) => {
-  User.findById(req.params.id)
-    .then((result) => {
-      res.render("user/view", { data: result, moment });
-    })
-    .catch((err) => console.error(err));
-});
+router.get("/view/:id", userController.user_view_get);
 
 // POST Requests
-
-router.post("/search", (req, res) => {
-  const searchText = req.body.searchText.trim();
-  User.find({ $or: [{ firstName: searchText }, { lastName: searchText }] })
-    .then((result) => {
-      // console.log(result);
-      res.render("user/search", { data: result, moment });
-    })
-    .catch((err) => console.error(err));
-});
+router.post("/search", userController.user_search_post);
 
 // DELETE Request
-router.delete("/edit/:id", (req, res) => {
-  User.findByIdAndDelete(req.params.id)
-    .then(() => {
-      res.redirect("/");
-    })
-    .catch((err) => console.error(err));
-});
+router.delete("/edit/:id", userController.user_delete);
 
 // PUT Request
-router.put("/edit/:id", (req, res) => {
-  User.updateOne({ _id: req.params.id }, req.body)
-    .then(() => {
-      res.redirect("/");
-    })
-    .catch((err) => console.error(err));
-});
+router.put("/edit/:id", userController.user_put);
 
 module.exports = router;
