06d4990db5ba9bce0ab5781ecbccafc8176ca342
Osama <elsultanmasr@gmail.com>
2025-12-06 12:39:34 +0200

    Lesson18 express router

--- app.js
+++ app.js
@@ -3,14 +3,12 @@ const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
 app.use(express.urlencoded({ extended: true }));
-const User = require("./models/customerSchema");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-var moment = require("moment");
 var methodOverride = require("method-override");
 app.use(methodOverride("_method"));
-// prettier-ignore
-const countries = ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda", "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cabo Verde", "Cambodia", "Cameroon", "Canada", "Central African Republic", "Chad", "Chile", "China", "Colombia", "Comoros", "Congo", "Costa Rica", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Democratic Republic of the Congo", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Eswatini", "Ethiopia", "Fiji", "Finland", "France", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Grenada", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Honduras", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Korea", "North Macedonia", "Norway", "Oman", "Pakistan", "Palau", "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Qatar", "Romania", "Russia", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Korea", "South Sudan", "Spain", "Sri Lanka", "Sudan", "Suriname", "Sweden", "Switzerland", "Syria", "Tajikistan", "Tanzania", "Thailand", "Timor-Leste", "Togo", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City", "Venezuela", "Vietnam", "Yemen", "Zambia", "Zimbabwe"]
+const allRoutes = require("./routes/allRoutes");
+const addUserRoute = require("./routes/addUser");
 
 // Auto Refresh
 const path = require("path");
@@ -27,71 +25,6 @@ liveReloadServer.server.once("connection", () => {
   }, 100);
 });
 
-// GET Requests
-app.get("/", (req, res) => {
-  //result => array of objects
-  User.find()
-    .then((result) => res.render("index", { data: result, moment }))
-    .catch((err) => console.error(err));
-});
-
-app.get("/user/add.html", (req, res) => {
-  res.render("user/add", { countries });
-});
-
-app.get("/edit/:id", (req, res) => {
-  User.findById(req.params.id)
-    .then((result) => {
-      res.render("user/edit", { data: result, moment, countries });
-    })
-    .catch((err) => console.error(err));
-});
-
-app.get("/view/:id", (req, res) => {
-  User.findById(req.params.id)
-    .then((result) => {
-      res.render("user/view", { data: result, moment });
-    })
-    .catch((err) => console.error(err));
-});
-
-// POST Requests
-app.post("/user/add.html", (req, res) => {
-  User.create(req.body)
-    .then(() => {
-      res.redirect("/");
-    })
-    .catch((err) => console.error(err));
-});
-
-app.post("/search", (req, res) => {
-  const searchText = req.body.searchText.trim();
-  User.find({ $or: [{ firstName: searchText }, { lastName: searchText }] })
-    .then((result) => {
-      // console.log(result);
-      res.render("user/search", { data: result, moment });
-    })
-    .catch((err) => console.error(err));
-});
-
-// DELETE Request
-app.delete("/edit/:id", (req, res) => {
-  User.findByIdAndDelete(req.params.id)
-    .then(() => {
-      res.redirect("/");
-    })
-    .catch((err) => console.error(err));
-});
-
-// PUT Request
-app.put("/edit/:id", (req, res) => {
-  User.updateOne({ _id: req.params.id }, req.body)
-    .then(() => {
-      res.redirect("/");
-    })
-    .catch((err) => console.error(err));
-});
-
 mongoose
   .connect(
     "mongodb+srv://elsultanmasr_db_user:DBpassword123@cluster0.xcbgw1z.mongodb.net/?appName=Cluster0"
@@ -104,3 +37,6 @@ mongoose
   .catch((err) => {
     console.error("Failed to connect to MongoDB", err);
   });
+
+app.use(allRoutes);
+app.use(addUserRoute);
