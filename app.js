a68c18b243d569f65067d7d27a6fff82fa38af9d
Osama <elsultanmasr@gmail.com>
2025-12-09 11:16:25 +0200

    Deploy

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const express = require("express");
-const mongoose = require("mongoose");
-var methodOverride = require("method-override");
-const path = require("path");
-
 const app = express();
 const port = process.env.PORT || 3000;
-
+const mongoose = require("mongoose");
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+var methodOverride = require("method-override");
 app.use(methodOverride("_method"));
-
 const allRoutes = require("./routes/allRoutes");
 const addUserRoute = require("./routes/addUser");
+
+// Auto Refresh
+const path = require("path");
+const livereload = require("livereload");
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, "public"));
 
 const connectLivereload = require("connect-livereload");
 app.use(connectLivereload());
@@ -23,6 +25,7 @@ liveReloadServer.server.once("connection", () => {
   }, 100);
 });
 
+require("dotenv").config(); // add this at the top
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
